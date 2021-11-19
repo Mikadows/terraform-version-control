a604532027716d330d23376939275162e073fdec
Gwendal Siwiorek <35867539+Mikadows@users.noreply.github.com>
2021-11-19 10:07:27 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+ //--------------------------------------------------------------------
+// Modules
+module "storageaccount" {
+  source  = "app.terraform.io/GwendalPerso/storageaccount/azurerm"
+  version = "0.0.1"
+
+  azure_location_vara = "West Europe"
+  azure_rg_name = "rgsiwiorek1111"
+  azure_storageaccount_name = "sa2409esgi5al11111999"
+  azure_storageaccount_replication = "LRS"
+  azure_storageaccount_tier = "Standard"
+}
